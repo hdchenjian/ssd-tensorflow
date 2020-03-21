1c319413b0cf03590d6bda81dc72aeaadef27da9
chen jian <jian.chen@iim.ltd>
2020-03-21 09:36:02 +0800

    add detect_image.py

---
+++ detect_image.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+from ssdutils import get_anchors_for_preset, decode_boxes, suppress_overlaps, get_preset_by_name
+from utils import draw_box
+from ssdvgg import SSDVGG
+
+def main():
+    checkpoint_file = 'ssd_train/e10.ckpt'
+    metagraph_file = checkpoint_file + '.meta'
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+
+        preset = get_preset_by_name('vgg300')
+        anchors = get_anchors_for_preset(preset)
+        net = SSDVGG(sess, preset)
+        net.build_from_metagraph(metagraph_file, checkpoint_file)
+                                
+        img_input = sess.graph.get_tensor_by_name('image_input:0')
+        result = sess.graph.get_tensor_by_name('result/result:0')
+
+        image_path = 'assets/test.jpg'
+        img = cv2.imread(image_path)
+        img = cv2.resize(img, (300, 300))
+        img = np.expand_dims(img, axis=0)
+        enc_boxes = sess.run(result, feed_dict={img_input: img})
+
+        lid2name = {0: 'Aeroplane', 1: 'Bicycle', 2: 'Bird', 3: 'Boat', 4: 'Bottle', 5: 'Bus', 6: 'Car',
+                    7: 'Cat', 8: 'Chair', 9: 'Cow', 10: 'Diningtable', 11: 'Dog', 12: 'Horse', 13: 'Motorbike',
+                    14: 'Person', 15: 'Pottedplant', 16: 'Sheep', 17: 'Sofa', 18: 'Train', 19: 'Tvmonitor'}
+        boxes = decode_boxes(enc_boxes[0], anchors, 0.5, lid2name, None)
+        boxes = suppress_overlaps(boxes)[:200]
+
+        img = cv2.imread(image_path)
+        for box in boxes:
+            color = (31, 119, 180)
+            draw_box(img, box[1], color)
+
+            box_data = '{} {} {} {} {} {}\n'.format(box[1].label, box[1].labelid,
+                                                    box[1].center.x, box[1].center.y, box[1].size.w, box[1].size.h)
+            print(box_data)
+        cv2.imwrite(image_path + '_out.jpg', img)
+
+if __name__ == '__main__':
+    main()
---
+++ setup_env.sh
@@ -0,0 +1,1 @@
+export LD_LIBRARY_PATH=/var/darknet/tf/cudnnv7.1/lib64:/usr/local/cuda/lib64
